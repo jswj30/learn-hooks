1e289148a39287b9c24b05821edb6fa7ce3e62b8
DESKTOP-VF6KUIN\UserK <jswj30@naver.com>
2021-08-23 15:47:54 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import Counter from './Counter';
+import Counter from './component/Counter';
+import './App.css';
 
 const App = () => {
-  // 
-  return <Counter />;
+  
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>useState</li>
+          <li>useEffect</li>
+        </ul>
+      </nav>
+      <hr />
+      <Counter />
+    </div>);
 }
 
 export default App;
